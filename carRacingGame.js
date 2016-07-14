52cfc98319bdd54ef5d308579ad29015e5b81278
Gaurav Joshi <gaurav@webcontentor.com>
2016-07-14 19:41:00 +0530

    dead code removed

--- carRacingGame.js
+++ carRacingGame.js
@@ -5,19 +5,11 @@ $(document).ready(function () {
         $('#header img').draggable();
         $('#bodyModel').droppable({
             accept: "#Component1",
-            drop: function (event, ui) {
-            var draggable = ui.draggable;
-            var offset = draggable.offset();
-            draggable.appendTo( this ).offset( offset );
-    }
-});
-        // $( function() {
-        //     $( "#header img" ).draggable();
-        //     $( "#bodyModel" ).droppable({
-        //         drop: function( event, ui ) {
-        //             $( this ).addClass( "ui-state-highlight" ).html( "Dropped!" );
-        //         }
-        //     });
-        // });
+            drop: function (event) {
+                var draggable = ui.draggable;
+                var offset = draggable.offset();
+                draggable.appendTo( this ).offset( offset );
+            }
+        });
     });
 });
