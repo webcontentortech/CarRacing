2c1408ac3dada3be65ffacdfcc977277706e9fdf
Gaurav Joshi <gaurav@webcontentor.com>
2016-07-19 17:46:25 +0530

    all task done

--- carRacingGame.js
+++ carRacingGame.js
@@ -1,22 +1,23 @@
 var imageName;
-var i=3;
+var RemainingChance=3;
+var jumping=false;
 
 $(document).ready(function () {
     $("#wrapper").hide();
-    $("#chance").text("RemainingChance:"+i);
+    $("#chance").text("RemainingChance:"+RemainingChance);
 
     $("#build").click(function () {
         buildingcar();
     });
 
     $("#jump").click(function () {
+        jumping=true;
         jump();
     });
 
     $("#run").click(function () {
         runCar();
     });
-
 
     function buildingcar() {
         $("#wrapper").show();
@@ -40,41 +41,28 @@ $(document).ready(function () {
 
     function runCar() {
         $("#header").hide();
-        $("#bodyModel").animate({ "marginLeft":"250px"},1000);
+        $("#bodyModel").animate({ "marginLeft":"250px"},2000);
         for (var i = 0; i < 100; i++) {
-            $("#bomb").animate({ "marginLeft":"-1200px"},4000, function () {
-                $("#bomb").hide();
-                });
-            $("#bomb").animate({ "marginLeft":"0px"},0, function () {
-                $("#bomb").show();
+            $("#bomb").animate({ "marginLeft":"-600px"},4000,function () {
+                if (jumping) {
+                    jumping = !jumping;
+                }else {
+                    collision();
+                }
             });
+            $("#bomb").animate({ "marginLeft":"0px"},0)      
         }
     }
 
-    function getPositions() {
-        var $box = $("#bodyModel");
-        var pos = $box.position();
-        var width = $box.width();
-        var height = $box.height();
-        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-        console.log(width,height);
-    }
-        
-    function comparePositions(p1, p2) {
-        var x1 = p1[0] < p2[0] ? p1 : p2;
-        var x2 = p1[0] < p2[0] ? p2 : p1;
-        return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
-        console.log(x1,x2);
-    }
-
-    function checkCollisions(){
-        var box = $(".bomb")[0];
-        var pos = getPositions(box);
-
-        var pos2 = getPositions(this);
-        var horizontalMatch = comparePositions(pos[0], pos2[0]);
-        var verticalMatch = comparePositions(pos[1], pos2[1]);            
-        var match = horizontalMatch && verticalMatch;
-        if (match) { $("body").append("<p>COLLISION !!!</p>"); }
+    function collision() {
+        alert("CAR CRASHED");
+        RemainingChance--;
+        $("#chance").text("RemainingChance:"+RemainingChance);
+        $("#bodyModel").animate({ "marginLeft":"0px"},"fast");
+        $("#bodyModel").animate({ "marginLeft":"250px"},500);
+        if (RemainingChance==0) {
+            alert("YOUR GAME IS OVER and BUILD YOUR CAR AGAIN and RUN IT AGAIN");
+            window.location.href = "index1.html";
+        }
     }
 });
