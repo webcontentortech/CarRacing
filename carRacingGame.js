0b7f619dbca153cce42fe0499d70bc97a171f44f
Gaurav Joshi <gaurav@webcontentor.com>
2016-07-28 14:31:10 +0530

    project completed

--- carRacingGame.js
+++ carRacingGame.js
@@ -1,14 +1,14 @@
 var imageName;
-var AvailableChance=3;
-var isCarJump=false;
-var userName="";
+var availableChance = 3;
+var isCarJump = false;
+var userName = "";
+var countDraggedParts = 0;
 
-var carDragableParts = {"backMirror": { isDragable: true, mainId: "backSideMirror", mainClass: "back-side-mirror", imageSrc: "mirror.png", imageId: "backMirror", imageClass: "back-mirror" },"frontMirror": { isDragable: true, mainId: "frontSideMirror", mainClass: "front-side-mirror", imageSrc: "frontmirror.png", imageId: "frontMirror", imageClass: "front-mirror" },"bodyFull": { isDragable: true, mainId: "bodyFull", mainClass: "body-full", imageSrc: "body.svg", imageId: "fullBody", imageClass: "full-body" },"tyre1": { isDragable: true, mainId: "tyre1", mainClass: "tyre-1", imageSrc: "tyre.png", imageId: "1tyre", imageClass: "first-tyre" },"tyre2": { isDragable: true, mainId: "tyre2", mainClass: "tyre-2", imageSrc: "tyre.png", imageId: "2tyre", imageClass: "second-tyre" },"light": { isDragable: true, mainId: "light", mainClass: "light", imageSrc: "headlight.png", imageId: "headLight", imageClass: "head-light" }}
 $(document).ready(function () {
-    $("#build,#run,#chance,#reset,#user,#header,#track1,#trackMovable,#track2,#name").hide();
+    $("#build,#run,#chance,#reset,#user,#header,#track1,#trackMovable,#track2,#name,#help").hide();
     $("#run").attr('disabled','disabled');
-    $("#chance").text("Available Chance: "+AvailableChance);
-    buttonColour();
+    $("#chance").text("Available Chance: " + availableChance);
+    
 
     $("#build").click(function () {
         $("#build").attr('disabled','disabled');
@@ -16,6 +16,7 @@ $(document).ready(function () {
     });
 
     $("#run").click(function () {
+        $("#run").attr('disabled','disabled');
         runCar();
     });
 
@@ -28,30 +29,28 @@ $(document).ready(function () {
     });
 
     $("#submit").click(function () {
-        userName = $("#userName").val();
-        ifInputIsEmpty();
+        ifInputIsEmpty(userName);
     });
 
     $(document).keydown(function(e){
         var key = e.which;
         if (key == "13") {
-            userName = $("#userName").val();
-            ifInputIsEmpty();
+            ifInputIsEmpty(userName);
         }
         if(key == "38") {
-            isCarJump=true;
             jump();
         }
     });
 
-    function buildingcar() {
+    var buildingcar = function () {
         $("#wrapper").show();
+        $("#help").hide();
         $("#header div").draggable();
         $("#bodyModel").droppable({
             drop: function (event,ui) {
                 $(this).append(ui.draggable);
-                imageName=ui.draggable.find('img').attr('src');
-                if (imageName == "frontmirror.png") {
+                countDraggedParts ++ ;
+                if (countDraggedParts == 6) {
                     alert("Your Car Is Ready To Run");
                     $("#run").removeAttr('disabled');
                 }
@@ -59,15 +58,16 @@ $(document).ready(function () {
         });
     }
 
-    function jump() {
+    var jump = function () {
+        isCarJump = true;
         $("#bodyModel").animate().stop();
         $("#bodyModel").animate({ top: "-120px" },{ duration: 500, easing: "easeOutQuad" });
         $("#bodyModel").animate({ top: "+10px" },{ duration: 2000, easing: "easeInQuad" });
     }
 
-    function runCar() {
+    var runCar = function () {
         $("#header,#blackBody").hide();
-        $("#bomb").show();
+        $("#bomb,#chance").show();
         $("#track1,#track2,#trackMovable").addClass("background-image");
         $("#bodyFull,#backMirror,#headLight,#frontMirror").addClass("bobbing-car-parts");
         $("#1tyre,#2tyre").addClass("spin-car-tyre");  
@@ -75,21 +75,25 @@ $(document).ready(function () {
         bombMovement(); 
     }
 
-    function collision() {
+    var collision = function () {
+        availableChance -- ;
+        $("#chance").text("availableChance: " + availableChance);
+        $("#bomb").animate({ "marginLeft":"0px"},"fast");
         alert("CAR CRASHED");
-        AvailableChance--;
-        console.log("CAR CRASHED after alert");
-        $("#chance").text("AvailableChance: "+ AvailableChance);
-        $("#bodyModel").animate({ "marginLeft":"0px"},"fast");
-        $("#bodyModel").animate({ "marginLeft":"250px"},500);
-        if (AvailableChance == 0) {
+        if (availableChance > 0) {
+            $("#bodyModel").animate({ "marginLeft":"0px"},"fast");
+            $("#bodyModel").animate({ "marginLeft":"250px"},1000);
+            setTimeout(function() {
+                bombMovement();
+            }, 50);
+        }
+        if (availableChance == 0) {
             alert("YOUR GAME IS OVER and BUILD YOUR CAR AGAIN and RUN IT AGAIN");
             reStart();
         }
-         bombMovement();
     }
 
-    function buttonColour() {
+    var givingColourToButons = function () {
         $("#readyToBuild,#submit").mouseover(function() {
             $("#readyToBuild,#submit").addClass("bgcolour");
         });
@@ -107,26 +111,31 @@ $(document).ready(function () {
         });
     }
 
-    function submitName() {
+    givingColourToButons();
+
+    var submitName = function () {
         $("#name").show();
+        $(".user-name").focus();
         $("#readyToBuild").hide();
     }
 
-    function submitUserName() {
+    var submitUserName = function (userName) {
         $("#body1,#bomb,#wrapper").hide();
-        $("#build,#run,#chance,#user,#header,#track1,#trackMovable,#track2,#reset").show();
-        $("#user").text("Welcome "+userName+"!");
+        $("#build,#run,#user,#header,#track1,#trackMovable,#track2,#reset").show();
     }
 
-    function ifInputIsEmpty() {
-        if($('#userName').val() == ''){
+    var ifInputIsEmpty = function (userName) {
+        $("#help").show();
+        userName = $("#userName").val();
+        if(userName == ''){
             alert("Please first enter your name");
         }else {
             submitUserName();
         }
+        $("#user").text("Welcome " + userName + "!");
     }
 
-    function bombMovement() {
+    var bombMovement = function () {
         $("#bomb").animate({ "marginLeft":"-600px"},4000,function () {
             if (isCarJump) {
                 $("#bomb").animate({ "marginLeft":"0px"},0,function () {
@@ -134,14 +143,12 @@ $(document).ready(function () {
                 });
                 isCarJump = !isCarJump;
             } else{
-                $("#bomb").animate({ "marginLeft":"0px"},0,function () {
-                    collision();
-                });
+                collision();
             }
         }); 
     }
 
-    function removeImages() {
+    var removeImages = function () {
         $("#bodyFull,#backSideMirror,#tyre1,#tyre2,#light,#frontSideMirror").remove();
         $("#backMirror,#fullBody,#1tyre,#2tyre,#headLight,#frontMirror").remove();
         $("#header").append("<div id='backSideMirror' class='back-side-mirror'><img src='mirror.png' id='backMirror' class='back-mirror'></div>");
@@ -152,17 +159,16 @@ $(document).ready(function () {
         $("#header").append("<div id='frontSideMirror' class='front-side-mirror'><img src='frontmirror.png' id='frontMirror' class='front-mirror'></div>");
     }
 
-    function reStart() {
+    var reStart = function () {
         $("#bomb").animate().stop();
         $("#bodyFull,#backMirror,#headLight,#frontMirror").removeClass("bobbing-car-parts");
         $("#1tyre,#2tyre").removeClass("spin-car-tyre");
         $("#track1,#track2,#trackMovable").removeClass("background-image");
         $("#bodyModel").animate({ "marginLeft":"0px"},"fast");
-        $("#header,#blackBody,#body1,#readyToBuild").show();
-        $("#bomb,#build,#run,#chance,#reset,#name,#user").hide();
+        $("#blackBody,#body1,#readyToBuild").show();
+        $("#bomb,#build,#run,#chance,#reset,#name,#user,#help,#header,#track1,#trackMovable,#track2").hide();
         removeImages();
-        $('#userName').val('');
-        $("#build,#run,#chance,#reset,#name,#user").hide();
+        $("#userName").val('');
         $("#run").attr('disabled','disabled');
         $("#build").removeAttr('disabled');
     }
